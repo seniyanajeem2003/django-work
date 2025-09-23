b0f085d10e5d53ab8eb139f98e55774518f5302a
Seniya Najeem <seniyanajeem1@gmail.com>
2025-09-23 19:33:10 +0530

    bookclub

---
+++ homework/home11/club/views.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+ 
+def signup_page(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+
+def login_page(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+@login_required(login_url='/login/')
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')
+
+    context = {
+        'user': request.user
+    }
+
+    return render(request, 'logout.html', context)
+
+
+
+@login_required(login_url='/login/')
+def home(request):
+    return render(request,'home.html')
+
+@login_required(login_url='/login/')
+def home(request):
+    count = request.session.get('page_count', 0)
+    count += 1
+    request.session['page_count'] = count
+    return render(request,'home.html', {'count': count})
